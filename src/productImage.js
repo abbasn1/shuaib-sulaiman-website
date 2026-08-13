768e11fcd2f1e17da55ba60691e630720ecc03d6
abbasn1 <abbaahmed@hotmail.com>
2026-08-13 20:10:15 +0100

    Replace product images with new S&S branded assets

--- src/productImage.js
+++ src/productImage.js
@@ -1,7 +1,10 @@
 const brandedProductImages = {
   's-s-shea-butter': '/images/products/ss-shea-butter.webp',
-  's-s-garri-cassava': '/images/products/ss-garri-white-ijebu.webp',
+  's-s-garri-cassava': '/images/products/ss-cassava-flour.webp',
   's-s-cashew-nut': '/images/products/ss-cashew-nut.webp',
+  's-s-charcoal': '/images/products/ss-charcoal.webp',
+  's-s-coal-black': '/images/products/ss-coal-black.webp',
+  's-s-yam-flour-amala': '/images/products/ss-yam-flour-amala.webp',
 }
 
 export function getProductImage(product) {
